88369e7df0cf35364c18906c3671377336d61343
Phodal HUANG <h@phodal.com>
2019-09-20 12:34:24 +0800

    fix: change controls

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,14 +11,14 @@ export class App {
     canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
   });
   private orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
-  private deviceOrientationControls = new DeviceOrientationControls(this.camera);
+  // @ts-ignore
+  private deviceOrientationControls: DeviceOrientationControls;
 
   constructor() {
     var loader = new GLTFLoader();
     var that = this;
 
     var lights = [];
-    var color = '#ff6842';
     lights[0] = new PointLight(0xff6842, 1, 0);
     lights[1] = new PointLight(0xffffff, .3, 0);
     lights[2] = new PointLight(0xffffff, .8, 0);
@@ -32,7 +32,7 @@ export class App {
     that.scene.add(lights[2]);
 
     loader.load(
-        "/assets/car.gltf.glb",
+        "/assets/car.gltf",
         function ( gltf ) {
           console.log(gltf.scene);
           that.scene.add(gltf.scene);
@@ -63,11 +63,20 @@ export class App {
     this.camera.lookAt(new Vector3(0, 0, 0));
 
     this.orbitControls.update();
+    window.addEventListener('deviceorientation', this.setDeviceOrientationControls, true);
 
     this.renderer.setSize(innerWidth, innerHeight);
     this.renderer.setClearColor(new Color('rgb(0,0,0)'));
 
     this.render();
+  }
+
+
+  setDeviceOrientationControls(e: any) {
+    this.deviceOrientationControls = new DeviceOrientationControls(this.camera);
+    this.deviceOrientationControls.connect();
+    this.deviceOrientationControls.update();
+    window.removeEventListener('deviceorientation', this.setDeviceOrientationControls, true);
   }
 
   private adjustCanvasSize() {
@@ -78,7 +87,7 @@ export class App {
 
   private render() {
     this.orbitControls.update();
-    this.deviceOrientationControls.update();
+    // this.deviceOrientationControls.update();
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(() => this.render());
 
